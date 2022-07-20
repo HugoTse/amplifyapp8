77da3fbc475223261e31a25d01ada1fd09f48a02
Hugo Tse <hugotse8@gmail.com>
2022-07-19 18:44:04 -0700

    added auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp8d44c2db7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Auth, Hub, API, Storage } from 'aws-amplify';
+import Amplify from 'aws-amplify';
+import config from './aws-exports.js';
+
+Amplify.configure(config);
 
 function App() {
+
+// For midway authentication
+const [user, setUser] = useState(null);
+
+// Midway Authentication
+useEffect(() => {
+  Hub.listen('auth', ({ payload: { event, data } }) => {
+    switch (event) {
+      case 'signIn':
+        console.log(event)
+        console.log(data)
+        getUser().then(userData => setUser(userData));
+        break;
+      case 'signOut':
+        setUser(null);
+        break;
+      case 'signIn_failure':
+        console.log('Sign in failure', data);
+        break;
+    }
+  });
+  getUser().then(userData => setUser(userData));
+}, []);
+
+function getUser() {
+  return Auth.currentAuthenticatedUser()
+    .then(userData => userData)
+    .catch(() => console.log('Not signed in'));
+}
+
+
+
+
+function getUser() {
+  return Auth.currentAuthenticatedUser()
+    .then(userData => userData)
+    .catch(() => console.log('Not signed in'));
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {user ? (
+      <>
+      <p>
+        User is signed in!
+      </p>
+
+      {/* Sign out button */}
+      <button 
+        onClick={() => Auth.signOut()}>Sign Out
+      </button>
+
+      </>
+    ) : (
+      <>
+      <p>
+        User is not signed in!
+      </p>
+
+      <button 
+        onClick={() => Auth.federatedSignIn({customProvider: "AmazonFederate"})}>Signin With Midway
+      </button>
+      
+      </>
+    )}
+   
+     
     </div>
   );
 }
